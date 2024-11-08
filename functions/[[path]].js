d1206a7c7a1531f29f14108febd5a15b2a9d0017
jeffnluhxp <jeffnluhxpceb@gmail.com>
2024-11-08 18:44:04 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://sinbeube.cloud'
+	const proxyUrl = 'https://minicutecatstki.xyz'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const headers = new Headers(request.headers)
